47d2e96cb4d1409670f36f19793aa0b326ccf545
Somit Agarwal <somitagarwal@Somits-MacBook-Air.local>
2026-02-02 14:56:32 +0530

    Day04 commit

--- Day14/higher_or_lower_game.py
+++ Day14/higher_or_lower_game.py
@@ -1,5 +1,29 @@
 import random
 
+def ans():
+    global score, condition
+    print("Compare A:",random1)
+    print("Against B:",random2)
+    choose = input("Choose between 'A' and 'B': ").upper()
+
+    if choose == "A":
+        if teams[random1] > teams[random2]:
+            score += 1
+            print("Correct guess. Your Current score is:",score)
+
+        else:
+            print("You lose. Your final score is:",score)
+            condition = False
+    elif choose == "B":
+        if teams[random1] < teams[random2]:
+            score += 1
+            print("Correct guess. Your score is",score)
+
+        else:
+            print("You lose. Your final score is:",score)
+            condition = False
+
+
 teams = {
     "csk":93, "mi":108, "rcb":105, "kkr":74, "gt":70, "pbks":66,
     "lsg":59, "dc":58, "srh":56, "rr":53
@@ -10,49 +34,17 @@ team_names = list(teams.keys())
 score = 0
 condition = True
 random1, random2 = random.sample(team_names, 2)
-print("Compare A:",random1)
-print("Against B:",random2)
-choose = input("Choose between 'A' and 'B': ").upper()
 
-if choose == "A":
-    if teams[random1] > teams[random2]:
-        score += 1
-        print("Correct guess. Your Current score is:",score)
-
-    else:
-        print("You lose. Your final score is:",score)
-elif choose == "B":
-    if teams[random1] < teams[random2]:
-        score += 1
-        print("Correct guess. Your score is",score)
-
-    else:
-        print("You lose. Your final score is:",score)
+ans()
 while condition:
 
     random1 = random2
     random2 = random.choice(team_names)
-    print("Compare A:",random1)
-    print("Against B:",random2) 
-    choose = input("Choose between 'A' and 'B': ").upper()
 
-    if choose == "A":
-        if teams[random1] > teams[random2]:
-            score += 1
-            print("Correct guess. Your Current score is:",score)
-            
-        else:
-            print("You lose. Your final score is:",score)
-            condition = False
-
-    elif choose == "B":
-        if teams[random1] < teams[random2]:
-            score += 1
-            print("Correct guess. Your score is",score)
-            
-        else:
-            print("You lose. Your final score is:",score)
-            condition = False
+    while random2 == random1:
+        random2 = random.choice(team_names)
+    
+    ans()
 
             
 
