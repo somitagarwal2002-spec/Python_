f50fe4c4c9a029cff11d3f9daa4b8b4a94b3bd31
Somit Agarwal <somitagarwal@Somits-MacBook-Air.local>
2026-02-02 22:38:41 +0530

    Day04 commit

---
+++ Day16/turle_basics.py
@@ -0,0 +1,25 @@
+# import turtle
+
+# timmy = turtle.Turtle() 
+
+
+# yha pe humne Turtle() bulayi hai na kyuki iska first alphabet capital 
+# hai jo show kr rha hai ki ye ek class hai
+
+# itna import krne se acha hum ye bhi likh skte hai
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle") # jo screen pr point aayega uska shape
+timmy.color("red") # changes color of the shape i.e turtle here
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
+
+my_screen.exitonclick()
+# jo screen hogi wo tb tk nhi jaegi jb tk hum uspe click na kare
+
