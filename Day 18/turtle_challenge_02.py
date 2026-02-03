96ccf318425398cb1f84e52a3c3a00ef1a6b9fd3
Somit Agarwal <somitagarwal@Somits-MacBook-Air.local>
2026-02-03 18:40:09 +0530

    Day05 commit

---
+++ Day 18/turtle_challenge_02.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+for i in range(10):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+
+screen = Screen()
+screen.exitonclick()
