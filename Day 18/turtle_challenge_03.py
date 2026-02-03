644e20b00abee3e120ea4bba38290eccf2084616
Somit Agarwal <somitagarwal@Somits-MacBook-Air.local>
2026-02-03 19:00:38 +0530

    Day05 commit

---
+++ Day 18/turtle_challenge_03.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen 
+import random
+
+timmy = Turtle()
+
+for n in range(3, 11):
+    angle = 180 - (180 * (n - 2)) / n
+    timmy.color(random.random(), random.random(), random.random())
+    for i in range(n):
+        timmy.forward(100)
+        timmy.right(angle)
+
+screen = Screen()   
+screen.exitonclick()
